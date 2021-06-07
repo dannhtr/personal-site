ce597dc3d25ff3403c97b9f79fc1eaf637a1108d
Daniel Camargo <decrdesign@gmail.com>
2021-06-07 19:56:10 -0400

    remove cra

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Heading } from "./heading"
-export { default as Text } from "./text"
-export { default as Container } from "./container"
-export { default as Nav } from "./nav"
-export { default as Social } from "./social"
-export { default as Orb } from "./orb"
