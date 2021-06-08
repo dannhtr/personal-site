c207d259ca9fa5cc93fbd7ab746d0aabd940a02e
Daniel Camargo <decrdesign@gmail.com>
2021-06-07 22:45:23 -0400

    next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const path = require('path');
+// const path = require('path');
 
 module.exports = {
-  distDir: 'build',
+  // distDir: 'build',
   target: 'serverless',
-  webpack(config) {
-    config.resolve.modules.push(path.resolve('./'));
-    return config;
-  },
+  // webpack(config) {
+  //   config.resolve.modules.push(path.resolve('./'));
+  //   return config;
+  // },
   future: {
     webpack5: true,
   },
