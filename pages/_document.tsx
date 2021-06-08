4688557a9100b1cc4cc9c5e2422dd4ca58bc67ee
Daniel Camargo <decrdesign@gmail.com>
2021-06-07 23:32:39 -0400

    document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,58 +2,43 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
-  // static async getInitialProps(ctx) {
-  //   const sheet = new ServerStyleSheet();
-  //   const originalRenderPage = ctx.renderPage;
-
-  //   try {
-  //     ctx.renderPage = () =>
-  //       originalRenderPage({
-  //         enchanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-  //       });
-  //     const initialProps = await NextDocument.getInitialProps(ctx);
-  //     return {
-  //       ...initialProps,
-  //       styles: (
-  //         <>
-  //           {initialProps.styles}
-  //           {sheet.getStyleElement()}
-  //         </>
-  //       ),
-  //     };
-  //   } finally {
-  //     sheet.seal();
-  //   }
-  // }
-
-  static getInitialProps({ renderPage }) {
-    // Step 1: Create an instance of ServerStyleSheet
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    // Step 2: Retrieve styles from components in the page
-    const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
-
-    // Step 3: Extract the styles as <style> tags
-    const styleTags = sheet.getStyleElement();
-
-    // Step 4: Pass styleTags as a prop
-    return { ...page, styleTags };
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enchanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link href="https://fonts.cdnfonts.com/css/tt-norms-pro" rel="stylesheet" />
-          {this.props.styles}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+  // render() {
+  //   return (
+  //     <Html>
+  //       <Head>
+  //         <link href="http://fonts.cdnfonts.com/css/tt-norms-pro" rel="stylesheet" />
+  //       </Head>
+  //       <body>
+  //         <Main />
+  //         <NextScript />
+  //       </body>
+  //     </Html>
+  //   );
+  // }
 }
 
 export default MyDocument;
