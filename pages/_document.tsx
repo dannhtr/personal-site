c09034654359db9156994c17a766a198350dafbb
Daniel Camargo <decrdesign@gmail.com>
2021-06-07 23:44:51 -0400

    document changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enchanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
         });
+
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
@@ -26,19 +27,19 @@ class MyDocument extends Document {
     }
   }
 
-  // render() {
-  //   return (
-  //     <Html>
-  //       <Head>
-  //         <link href="http://fonts.cdnfonts.com/css/tt-norms-pro" rel="stylesheet" />
-  //       </Head>
-  //       <body>
-  //         <Main />
-  //         <NextScript />
-  //       </body>
-  //     </Html>
-  //   );
-  // }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href="https://fonts.cdnfonts.com/css/tt-norms-pro" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
